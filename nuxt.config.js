208947285d75ab72f2e8223912cbe1fe32366c98
yashtiwari <42347292+yashtiwari@users.noreply.github.com>
2020-04-20 16:16:30 +0530

    change url
    change URL for test

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 let dynamicRoutes = () => {
   const routes = axios
-    .get("https://css-tricks.com/wp-json/wp/v2/posts?page=1&per_page=20")
+    .get("http://dev-galaxy-test-site.pantheonsite.io/wp-json/wp/v2/posts?page=1&per_page=20")
     .then(res => {
       return res.data.map(post => `/blog/${post.slug}`)
     })
